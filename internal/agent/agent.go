ab8b7b7c624b7b8d096c3b36917ee6b6099a8971
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2024-08-13 20:00:12 +0400

    implements metrics collection

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -2,8 +2,53 @@ package agent
 
 import (
 	"fmt"
+	"math/rand"
+	"reflect"
+	"runtime"
 	"time"
 )
+
+var needfulMemStats = [...]string{
+	"Alloc",
+	"BuckHashSys",
+	"Frees",
+	"GCCPUFraction",
+	"GCSys",
+	"HeapAlloc",
+	"HeapIdle",
+	"HeapInuse",
+	"HeapObjects",
+	"HeapReleased",
+	"HeapSys",
+	"LastGC",
+	"Lookups",
+	"MCacheInuse",
+	"MCacheSys",
+	"MSpanInuse",
+	"MSpanSys",
+	"Mallocs",
+	"NextGC",
+	"NumForcedGC",
+	"NumGC",
+	"OtherSys",
+	"PauseTotalNs",
+	"StackInuse",
+	"StackSys",
+	"Sys",
+	"TotalAlloc",
+}
+
+type Metric struct {
+	mType string
+	name  string
+	value string
+}
+
+type Metrics struct {
+	pollCount   int64
+	randomValue float64
+	list        map[string]Metric
+}
 
 type AgentConfig struct {
 	serverAddress  string
@@ -13,12 +58,25 @@ type AgentConfig struct {
 }
 
 type Agent struct {
+	memStats runtime.MemStats
+	metrics  Metrics
 }
+
+const pollCount = "PollCount"
+const randomValue = "RandomValue"
 
 func NewAgent() *Agent {
 	fmt.Printf("Agent created.\r\n")
 
-	return &Agent{}
+	var metrics = Metrics{
+		pollCount:   0,
+		randomValue: 0,
+		list:        make(map[string]Metric),
+	}
+
+	return &Agent{
+		metrics: metrics,
+	}
 }
 
 func (agent *Agent) Run() {
@@ -48,6 +106,30 @@ func (agent *Agent) startSending() {
 
 func (agent *Agent) updateMetrics() {
 	fmt.Printf("Agent updated metrics.\r\n")
+
+	runtime.ReadMemStats(&agent.memStats)
+	reflectedMemStats := reflect.ValueOf(agent.memStats)
+
+	// I want it to be simple: I have the list of needed metrics, so I just loop over the MemStats
+	// to get only those values I need.
+	// I know that some people use JSON to bypass Go limitation on dynamic field name lookup in structs,
+	// but I'm doing it in my way ;)
+	for _, mName := range needfulMemStats {
+		value := reflect.Indirect(reflectedMemStats).FieldByName(mName)
+		// fmt.Printf("%v=%v\r\n", mName, value)
+
+		if metric, present := agent.metrics.list[mName]; present {
+			metric.value = fmt.Sprintf("%v", value)
+			agent.metrics.list[mName] = metric
+		} else {
+			agent.metrics.list[mName] = Metric{"gauge", mName, fmt.Sprintf("%v", value)}
+		}
+	}
+
+	agent.metrics.pollCount++
+	agent.metrics.randomValue = rand.Float64()
+
+	// fmt.Printf("Metrics: %v\r\n", agent.metrics)
 }
 
 func (agent *Agent) sendMetrics() {
