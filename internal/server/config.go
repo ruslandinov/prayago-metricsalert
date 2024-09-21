a1365cf9b31da16f885b60c6d548943110d845e7
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2024-09-21 09:36:51 +0400

    adds db connection string param to server config

--- internal/server/config.go
+++ internal/server/config.go
@@ -11,6 +11,7 @@ import (
 type ServerConfig struct {
 	ServerAddress         string
 	StorageFPath          string
+	DBConnectionString    string
 	StoreInterval         time.Duration
 	RestoreStorageOnStart bool
 }
@@ -18,11 +19,18 @@ type ServerConfig struct {
 func NewServerConfig() ServerConfig {
 	a := flag.String("a", "localhost:8080", "server address and port")
 	f := flag.String("f", "./storage.json", "memstorage file path")
+	d := flag.String("d", "", "database connection string")
 	i := flag.Int("i", 300, "memstorage saving interval, sec")
 	r := flag.Bool("r", true, "should memstorage restore values on server start or not")
 	flag.Parse()
 
-	config := ServerConfig{*a, *f, time.Duration(*i) * time.Second, *r}
+	config := ServerConfig{
+		ServerAddress: *a,
+		StorageFPath: *f,
+		DBConnectionString: *d,
+		StoreInterval: time.Duration(*i) * time.Second,
+		RestoreStorageOnStart: *r,
+	}
 
 	if envServerAddress := os.Getenv("ADDRESS"); envServerAddress != "" {
 		config.ServerAddress = envServerAddress
@@ -37,6 +45,9 @@ func NewServerConfig() ServerConfig {
 	}
 	if envRestoreStorageOnStart := os.Getenv("RESTORE"); envRestoreStorageOnStart != "" {
 		config.RestoreStorageOnStart, _ = strconv.ParseBool(envRestoreStorageOnStart)
+	}
+	if envDataBaseDsn := os.Getenv("DATABASE_DSN"); envDataBaseDsn != "" {
+		config.DBConnectionString = envDataBaseDsn
 	}
 
 	logger.LogSugar.Infoln("Server config:", config)
